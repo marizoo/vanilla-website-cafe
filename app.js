f05a85b53df7839c5a6a676443994759ee94177a
marizoo <marizoo@meow.woof>
2022-02-20 18:43:04 +0700

    section: Products: Filtering: 100%

--- app.js
+++ app.js
@@ -42,10 +42,27 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
-
+let mixerProducts = mixitup(".products__content", {
+    selectors: {
+        target: ".products__card",
+    },
+    animation: {
+        duration: 300,
+    },
+});
 /* Default filter products */
+mixerProducts.filter(".delicacies");
 
 /* Link active products */
+// target the title
+const linkProducts = document.querySelectorAll(".products__item");
+
+function activeProducts() {
+    linkProducts.forEach((title) => title.classList.remove("active-product"));
+    this.classList.add("active-product");
+}
+
+linkProducts.forEach((l) => l.addEventListener("click", activeProducts));
 
 /*=============== SHOW SCROLL UP ===============*/
 
