f2d4abb103a0bd68be4120e6590ca0ce29812aab
marizoo <marizoo@meow.woof>
2022-02-21 17:39:10 +0700

    completed the project

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 /*=============== LOADER ===============*/
+onload = () => {
+    const load = document.getElementById("load");
+
+    setTimeout(() => {
+        load.style.display = "none";
+    }, 2500);
+};
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu");
@@ -65,5 +72,44 @@ function activeProducts() {
 linkProducts.forEach((l) => l.addEventListener("click", activeProducts));
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+    // When the scroll is higher than 350vh, add the 'show-scroll' class to the 'a' tag, with the 'scroll-top' class
+    if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+// ============ SHOW CART ===============
+const cart = document.getElementById("cart");
+const cartShop = document.getElementById("cart-shop");
+const cartClose = document.getElementById("cart-close");
+// === CART SHOW ===
+// Validate if constant exist
+if (cartShop) {
+    cartShop.addEventListener("click", () => {});
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                .classList.remove("active-link");
+        }
+    });
+}
+window.addEventListener("scroll", scrollActive);
